4be86c04ff5a9ba816ec1ec54ba3deb66b2f0c38
Akshata2298 <a.k.akshata96@gmail.com>
2022-02-11 19:04:51 +0530

    Basic codes of Java
    by Akshata

---
+++ corejava/src/corejava/DataTypes.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b,c,d;
+		b=20;
+		c=a+b;
+		d=a-b;
+		System.out.println("Add="+c);
+		System.out.println("Sub="+d);
+	}
+
+}
